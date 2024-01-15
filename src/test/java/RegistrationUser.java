bbed2c4eee807aa99458e9996b3d7806b3e86f74
AndPrim <andreyminoss@gmail.com>
2024-01-15 15:09:12 +0500

    FinalCommit

--- src/test/java/RegistrationUser.java
+++ src/test/java/RegistrationUser.java
@@ -7,25 +7,12 @@ public class RegistrationUser {
         return login;
     }
 
-    public void setLogin(String login) {
-        this.login = login;
-    }
+
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 
 
 
